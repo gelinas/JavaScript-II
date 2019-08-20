ff674a0ad3ed9972335ea115c6e40c92768eabbc
gelinas <jlgelinas@gmail.com>
2019-08-20 16:36:47 -0400

    completed array-methods.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,14 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+let globalVariable = 3;
+
+function addUp(num) {
+  sum = globalVariable + num;
+  return sum;
+}
+console.log(addUp(4));
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
